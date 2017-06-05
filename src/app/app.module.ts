056492d9bd7b515294b6b2212d1a64e21d9c20c5
James Duff <jaoduff@gmail.com>
2017-06-05 16:45:10 +0800

    improved signup-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import {zippyComponent } from './components/zippy/zippy.component';
 import { subscribeFormComponent } from './components/subscribeform/subscribeform.component';
@@ -20,7 +20,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/signup-form/signup-form.component.ts
+++ src/app/components/signup-form/signup-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators, NgForm } from '@angular/forms';
 
 @Component ({
     selector: 'signup',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
     styles: [`form { padding: 20px; }`]
 })
 export class signupFormComponent {
-
+    form = new FormGroup({
+        username: new FormControl('',Validators.required),
+        password: new FormControl('', Validators.required)
+    });
+    singup() {
+        console.log(this.form.value);
+    }
 }
